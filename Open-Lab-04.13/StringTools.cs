29ba2dda66e49f0fc2dbbb8bc9789bdbbef13c46
AdamHaluska <adam.haluska11@gmail.com>
2021-11-21 23:02:23 +0100

    hotovo!

--- Open-Lab-04.13/StringTools.cs
+++ Open-Lab-04.13/StringTools.cs
@@ -6,7 +6,29 @@ namespace Open_Lab_04._13
     {
         public string GetLongestCommonSequence(string str1, string str2)
         {
-            throw new NotImplementedException();
+              var Dlzky = new int[str1.Length, str2.Length];
+            int najDlzka = 0;
+            string output = "";
+            for (int i = 0; i < str1.Length; i++)
+            {
+                for (int j = 0; j < str2.Length; j++)
+                {
+                    if (str1[i] == str2[j])
+                    {
+                        Dlzky[i, j] = i == 0 || j == 0 ? 1 : Dlzky[i - 1, j - 1] + 1;
+                        if (Dlzky[i, j] > najDlzka)
+                        {
+                            najDlzka = Dlzky[i, j];
+                            output = str1.Substring(i - najDlzka + 1, najDlzka);
+                        }
+                    }
+                    else
+                    {
+                        Dlzky[i, j] = 0;
+                    }
+                }
+            }
+            return output;
         }
     }
 }
